8be317c4be7b5920489f69a9c5f1e7dd0a6eea3c
Victor Shaviya <vs610642@digicelgroup.com>
2022-08-13 13:22:41 -0500

    Create the constants for the loader functon

---
+++ assets/logic.js
@@ -0,0 +1,4 @@
+const cardImages = document.querySelectorAll('.card-image');
+const cardTitles = document.querySelectorAll('.card-title');
+const cardDescriptions = document.querySelectorAll('.card-description');
+const cardMediaIcons = document.querySelectorAll('.card-mediaIcons a');
