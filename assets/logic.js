f18dafd17b2026dca7b33fbf0bd983bed8f25a45
Victor Shaviya <vs610642@digicelgroup.com>
2022-08-13 13:48:10 -0500

    Add constants for the span selectors

--- assets/logic.js
+++ assets/logic.js
@@ -3,6 +3,11 @@ const cardTitles = document.querySelectorAll('.card-title');
 const cardDescriptions = document.querySelectorAll('.card-description');
 const cardMediaIcons = document.querySelectorAll('.card-mediaIcons a');
 
+const cardImgs = document.querySelectorAll('.card-image img');
+const cardTitleSpan = document.querySelectorAll('.card-title span');
+const cardDescriptionSpan = document.querySelectorAll('.card-description span');
+const MediaIcon = document.querySelectorAll('.card-mediaIcons a img');
+
 const renderCard = () => {
   // removing the skeleton loading effect
   cardImages.forEach((cardImage) => {
