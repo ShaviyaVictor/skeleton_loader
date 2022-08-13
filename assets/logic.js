463bad515bd1d721940c024252c1c71ff767338f
Victor Shaviya <vs610642@digicelgroup.com>
2022-08-13 13:30:14 -0500

    Create a forEach function to remove the loading classList

--- assets/logic.js
+++ assets/logic.js
@@ -2,3 +2,19 @@ const cardImages = document.querySelectorAll('.card-image');
 const cardTitles = document.querySelectorAll('.card-title');
 const cardDescriptions = document.querySelectorAll('.card-description');
 const cardMediaIcons = document.querySelectorAll('.card-mediaIcons a');
+
+const renderCard = () => {
+  // removing the skeleton loading effect
+  cardImages.forEach((cardImage) => {
+    cardImage.classList.remove('loading');
+  });
+  cardTitles.forEach((cardTitle) => {
+    cardTitle.classList.remove('loading');
+  });
+  cardDescriptions.forEach((cardDescription) => {
+    cardDescription.classList.remove('loading');
+  });
+  cardMediaIcons.forEach((cardMediaIcon) => {
+    cardMediaIcon.classList.remove('loading');
+  });
+}
